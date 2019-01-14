3c358a3f0922bd3c79df5ca031b0394c72eac29b
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-14 22:37:10 +0530

    solution for Problem 1

---
+++ 1.FindLenght/test_find_lenght.py
@@ -0,0 +1,13 @@
+import FindLenght
+
+def test_AssertTrue():
+    assert True
+
+def test_array_retutn_5():
+    #arrange
+    array = [1,5,2,8,9]
+    excepted = 5
+    #act
+    actual = FindLenght.count(array)
+    #assert
+    assert excepted == actual
