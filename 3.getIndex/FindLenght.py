ec31f533de0a380699579ebf401e5035b0c4dc11
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-14 23:19:53 +0530

    Solutio for Problem 3

---
+++ 3.getIndex/FindLenght.py
@@ -0,0 +1,11 @@
+#To find the lenght of an array using slicing methodogy
+
+def count(a):
+    counti = 0
+    while(a[counti:]):
+        counti += 1
+
+    return counti
+
+array = [1,5,2,8,9]
+print(count(array)) 
---
+++ 3.getIndex/getIndex.py
@@ -0,0 +1,17 @@
+#3. Given an unsorted integer array A and an integer value X,
+#  return the index at which X is located in A or return -1 if it is not found in A.
+#import FindLenght
+def getIndex(array,indexFor):
+    index = 0
+    i = 0
+    lennght = len(array)
+    while (i!=lennght):
+        if (array[i] == indexFor):
+            break
+        index += 1
+        i += 1
+    return index
+
+array = [7,4,5,7]
+indexFor = 5
+print(getIndex(array,indexFor))
