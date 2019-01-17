5a72dcb5bbde0abe560e72fcb5410b0b3f8413bf
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-17 22:23:00 +0530

    solution for problem 17

---
+++ 17.reverseArrayElements/reverse_Array_elements.py
@@ -0,0 +1,13 @@
+#17. Write a function that takes an unsorted integer
+#  array as input and prints the contents of the array in reverse order.
+import FindLenght
+def reverseContentofAnArray(array):
+    i = 1
+    lenghtOfArray = FindLenght.count(array)
+    print("Printing the Content of the array in reverse order ")
+    while (i!=lenghtOfArray):
+        print(array[-i])
+        i += 1
+    print(array[0])
+array = [2,1,5,7,3]
+reverseContentofAnArray(array)
