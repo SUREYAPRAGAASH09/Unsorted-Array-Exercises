86247b4bf97d712034af21102602fec7c544d58b
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-19 23:20:07 +0530

    solution for problem 26

---
+++ 26.Find_kth_Largest_ele/find_kth_Largest_ele.py
@@ -0,0 +1,24 @@
+import Max
+import FindLenght
+import find_min
+def kth_largest_ele(array,kthele):
+    lenght = FindLenght.count(array)
+    if (lenght == kthele):
+        return find_min.findMin(array)
+    else:
+
+        iterator = 1
+        while(iterator!=kthele):
+            Largest_ele = Max.Max(array)
+            for j in array:
+                if (j == Largest_ele):
+                    array.remove(Largest_ele)
+            iterator += 1
+        Largest_ele = Max.Max(array)
+    return Largest_ele
+
+array = [5,1,9,7,2,8,3,6,4]
+kthele = 1
+#print()
+v = kth_largest_ele(array,kthele)
+print("The {0} Largest Element in the array is {1}".format(kthele,v))
