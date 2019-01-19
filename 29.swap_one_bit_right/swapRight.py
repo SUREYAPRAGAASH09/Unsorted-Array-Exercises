a157bcea068923035c4565276024a7bd47355bf4
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-19 23:23:24 +0530

    solution for problem 29

---
+++ 29.swap_one_bit_right/swapRight.py
@@ -0,0 +1,16 @@
+def swapRight(array):
+    
+    
+    temp = 0
+    j = 0
+    while (j!=1):
+        i = -1
+        while (i!=len(array)):
+            array[i],temp = temp,array[i]
+            i += 1
+        j += 1
+    return array
+
+array = [5,7,6,9,2,8]
+print(array)
+print(swapRight(array))
