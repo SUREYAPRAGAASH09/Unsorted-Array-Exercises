8d4da55a3668f7aa8fdf0b99b4efbd31191f9981
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-20 22:21:29 +0530

    solution for problem 33

---
+++ 33.shiftGreaterThanN/shiftGreaterN.py
@@ -0,0 +1,35 @@
+def swapRight(array,shiftValue):
+    if (shiftValue > len(array)):
+        shiftValue = shiftValue - len(array)
+    
+    temp = 0
+    j = 0
+    while (j!=shiftValue):
+        i = -1
+        while (i!=len(array)):
+            array[i],temp = temp,array[i]
+            i += 1
+        j += 1
+    return array
+
+def shiftLeft(array,shiftValue):
+    if (shiftValue > len(array)):
+        shiftValue = shiftValue - len(array)
+    i = 0
+    while(i!=shiftValue):
+        j = 0
+        temp = (array[j])
+        array.remove(array[j])
+        array.append(temp)
+        i+=1
+    return array
+
+
+array = [5,7,6,9,2,8]
+shiftValue = 10
+print(array)
+print("Shift Right")
+print(swapRight(array,shiftValue))
+print("Shift Left")
+array = [5,7,6,9,2,8]
+print(shiftLeft(array,shiftValue))
