3d5102ae0f697bdc207c8292811f41f6f6545c78
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-20 22:20:57 +0530

    solution for problem 32

---
+++ 32.shiftrightleft/shiftrightleft.py
@@ -0,0 +1,30 @@
+def swapRight(array,shiftValue):
+    temp = 0
+    j = 0
+    while (j!=shiftValue):
+        i = -1
+        while (i!=len(array)):
+            array[i],temp = temp,array[i]
+            i += 1
+        j += 1
+    return array
+
+def shiftLeft(array,shiftValue):
+    i = 0
+    while(i!=shiftValue):
+        j = 0
+        temp = (array[j])
+        array.remove(array[j])
+        array.append(temp)
+        i+=1
+    return array
+
+
+array = [5,7,6,9,2,8]
+shiftValue = 5
+print(array)
+print("Shift Right")
+print(swapRight(array,shiftValue))
+print("Shift Left")
+array = [5,7,6,9,2,8]
+print(shiftLeft(array,shiftValue))
