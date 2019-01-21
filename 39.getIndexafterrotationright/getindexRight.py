ecf519a382f027e9df9402cd130056a329704337
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-21 07:57:40 +0530

    solution for problem 39

---
+++ 39.getIndexafterrotationright/getindexRight.py
@@ -0,0 +1,12 @@
+#37. Given an unsorted integer array A, 
+# find the value that will be in 3rd position or index after 2 rotations to the right.
+import Rotateright
+
+def getIndexAfterrotationRight(array,index,rotation_value):
+    afterRotation = Rotateright.swapRight(array,rotation_value)
+    return afterRotation[index]
+
+array = [5,7,6,9,2,8]
+index = int(input("Enter the index value"))
+rotation_value = int(input("Enter the rotation value"))
+print(getIndexAfterrotationRight(array,index,rotation_value))
