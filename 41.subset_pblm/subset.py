6dd25ae36954750de8629d60205a3c4c4286cf50
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-21 07:59:06 +0530

    solution for problem 41

---
+++ 41.subset_pblm/subset.py
@@ -0,0 +1,15 @@
+import FindLenght
+def subset(array,sum):    
+    i = 0
+    lenght = FindLenght.count(array)
+    while (i!=lenght):
+        j = i + 1
+        while (j!=lenght):
+            if ((array[i] + array[j]) == sum):
+                print(array[i],array[j])
+            j += 1
+        i += 1
+
+array = [5,7,8,2,6,5,8]
+sum = 16
+subset(array,sum)
