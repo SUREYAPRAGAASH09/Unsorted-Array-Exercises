b5bdaeb7e531dd3cb1fc9bea48935e7decaa56c9
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-22 22:38:49 +0530

    test file updated

--- 2.iscontains/test_isContain.py
+++ 2.iscontains/test_isContain.py
@@ -12,4 +12,14 @@ def test_array_retutn_5():
     #act
     actual = isContain.find_element(array,Search_element)
     #assert
+    assert excepted == actual 
+
+def test_array1_retutn_5():
+    #arrange
+    array = [3,5,7,9,0,3]
+    Search_element = 9
+    excepted = True
+    #act
+    actual = isContain.find_element(array,Search_element)
+    #assert
     assert excepted == actual
