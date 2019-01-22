8a41efb71b365a998e539a040599b4ce5189c89a
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-22 22:42:49 +0530

    test file updated

--- 7.sum/test_sum.py
+++ 7.sum/test_sum.py
@@ -10,4 +10,13 @@ def test_arrayreturn25():
     #act
     output = sum_elements.sum(array)
     #assert
+    assert excepted == output 
+
+def test1_arrayreturn25():
+    #arrange
+    array = [1,5,3,6,7,5]
+    excepted = 27
+    #act
+    output = sum_elements.sum(array)
+    #assert
     assert excepted == output
