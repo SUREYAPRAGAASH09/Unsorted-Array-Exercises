92f59440a94b0350a488808544488e8f6b221f76
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-25 19:36:29 +0530

    for rough use purpose

---
+++ mode_array.py
@@ -0,0 +1,14 @@
+
+
+def lenght(array):
+    d = {}
+    for i in array:
+        #count = 1
+        key = i
+        d[key] = 1 
+        #if ()
+    print(d)
+
+array = [1,2,2,2,3,4,5,5]
+lenght(array)
+        
