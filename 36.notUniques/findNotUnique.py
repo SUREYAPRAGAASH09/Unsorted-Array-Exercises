853e5db0f763836694bc302b1d225fcbd93d1d5e
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-30 22:41:32 +0530

    soltuion updated

--- 36.notUniques/findNotUnique.py
+++ 36.notUniques/findNotUnique.py
@@ -3,15 +3,17 @@
 import find_min
 import dictionaryOperations
 def findUnique(array):
+    not_unique = []
     dictionary = dictionaryOperations.built_dictionary(array)
     listValues = list(dictionary.values())
     minimum_value = find_min.findMin(listValues)
     for i in listValues:
         if i != minimum_value :
             b = (dictionaryOperations.find_key_from_dictionary(dictionary,i))
-            print(b,i)
+            not_unique.append(b)
             dictionary.pop(b)
+    return not_unique
 
 array = [5,7,8,2,6,5,8]
-findUnique(array)
+print(findUnique(array))
 #print(built_dictionary(array)) 
