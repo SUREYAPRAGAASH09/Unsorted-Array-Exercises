8787d4bede5efe15be33d0f0a08b965a6c126dbb
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-30 22:29:36 +0530

    test file updated

--- 40.getIndexafterroationleft/test_getindexafterrotation.py
+++ 40.getIndexafterroationleft/test_getindexafterrotation.py
@@ -12,3 +12,14 @@ def test_indexvalue_rotationvalue_return_9():
     actual = getindexAterrotation.getIndexAfterrotationLeft(array,index,rotation_value)
     #assert
     assert excepted == actual 
+
+def test_indexvalue_rotationvalue_return_6():
+    #arrange
+    array = [5,7,6,9,2,8]
+    index = 1
+    rotation_value = 1
+    excepted = 6
+    #act
+    actual = getindexAterrotation.getIndexAfterrotationLeft(array,index,rotation_value)
+    #assert
+    assert excepted == actual 
