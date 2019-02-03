79b5efb10215cb97584b897dee1688ae85586916
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-03 21:36:06 +0530

    Code revised

--- 10.Find_Min/find_min.py
+++ 10.Find_Min/find_min.py
@@ -1,21 +1,33 @@
- # To find the min of dictionary values 
+Problem :
+=========
+ To find the min of dictionary values
+Input :
+=======
+    Unsorted Array Elements
+Output :
+========
+    Minimum Element in an Unsorted Array
+My Way of Solving the Problem (Algorithm) :
+===========================================
+    1.Find the lenght of the array using Findlenght function written by me.
+    2.Set the updated minumum variable by comparing the first two elements in the array
+    3.using while loop update updated minimum value untill iterating the entrire array
+Code :
+======
 import FindLenght
-def findMin(values):
-    print()
-    nooo
-    values_count = FindLenght.count(values) 
-    one = values[0] 
-    two = values[1] 
-    if (one < two): 
-        min = one 
+def findMin(Array):
+    
+    Array_lenght = FindLenght.len(values) 
+     
+    if (Array[0] < Array[1]): 
+        Updated_minimum  = Array[0]
     else: 
-        min = two 
-    i = 2 
-    while(i!=values_count): 
-        if (values[i]<min): 
-            min = values[i] 
-        i += 1 
-    return min 
+        Updated_minimum = Array[1]
+    iterator = 2 
+    while(iterator!=Array_lenght): 
+        if (Array[iterator]<Updated_minimum): 
+            Updated_minimum = Array[iterator] 
+        iterator += 1 
+    return Updated_minimum 
 
-#values = [5,0,1,7,3,5]
-#print(findMin(values))
+
