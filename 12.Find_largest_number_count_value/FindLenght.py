f07452bba17388f6bbc9a33d42172002d644c0b8
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-04 11:23:33 +0530

    Code Revised Completely

--- 12.Find_largest_number_count_value/FindLenght.py
+++ 12.Find_largest_number_count_value/FindLenght.py
@@ -1,11 +1,25 @@
-#To find the lenght of an array using slicing methodogy
+Question :
+==========
+    To find the lenght of an array using slicing methodogy
+Input :
+=======
+    Unsorted Array Elements
 
-def count(a):
-    counti = 0
-    while(a[counti:]):
-        counti += 1
+Output:
+=======
+    Count the No of Element in the Array
 
-    return counti
+Algorithm:
+==========
 
-#array = [1,5,2,8,9]
-#print(count(array)) 
+    1.Initialize the Cout variable to 0
+    2.Using the array Slicing concepts each and every element in an array is taken using While loop and the Count is incremented by 1
+    3.print the count variable 
+
+Code:
+=====
+def FindLenght(array):
+    count = 0
+    while(a[count:]):
+        count += 1    
+    return count
