9e92d52f15f1606e9ad264f58d4671fbe97468bc
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-04 15:45:20 +0530

    Code Revised Completely

--- 13.findSmallnumberCount/findSmallestNumberCount.py
+++ 13.findSmallnumberCount/findSmallestNumberCount.py
@@ -1,20 +1,32 @@
-#12. Given an unsorted integer array A, 
-# find the number of times the smallest number is found in the array.
-import find_min
+Question :
+==========
+     Given an unsorted integer array A,find the number of times the smallest number is found in the array.
+Input :
+=======
+    Unsorted Array Elements
+Output :
+========
+    Get the Smallest Number occurence and it's Count value
+Algorithm :
+===========
+     1.Get the minumum number form the array 
+     2.using the count value , count the occruence of that minimum elemet found in that array 
+     3.return the count 
+Code :
+======
+import Findmin
 import FindLenght
-def count_occuences(array,min):
+def count_a_particular_number(array,min): #max is the number that we have to count 
     count = 0
-    i = 0
+    iterator = 0
     lenght = FindLenght.count(array)
-    while (i!=lenght):
-        if (array[i]==min):
+    while (iterator!=lenght):
+        if (array[iterator]==max):
             count += 1
-        i += 1
+        iterator += 1
     return count
 
-def find_Smallest_numbers_occurences(array):
-    min = find_min.find_smallest_value(array)
+def find_largest_numbers_occurences(array): #Main method 
+    min = Findmin.findmin(array)
     count = count_occuences(array,min)
     return count
-array = [3,8,3,7,9]
-print(find_Smallest_numbers_occurences(array))
