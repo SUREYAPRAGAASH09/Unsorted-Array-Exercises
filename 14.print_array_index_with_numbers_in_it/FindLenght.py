ba51ce2f7e5bc2be23e7fce0b98d02500a7a060c
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-04 15:47:38 +0530

    Code Revised Completely

---
+++ 14.print_array_index_with_numbers_in_it/FindLenght.py
@@ -0,0 +1,24 @@
+Question :
+==========
+    To find the lenght of an array using slicing methodogy
+Input :
+=======
+    Unsorted Array Elements
+Output:
+=======
+    Count the No of Element in the Array
+Algorithm:
+==========
+    1.Initialize the Cout variable to 0
+    2.Using the array Slicing concepts each and every element in an array is taken using While loop and the Count is incremented by 1
+    3.print the count variable 
+Code:
+=====
+def FindLenght(array):
+    count = 0
+    while(a[count:]):
+        count += 1    
+ 
+    return count
+
+ 
