bbc78f7a1439d97f73d7c0704e1014b4f950d9af
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-05 22:22:05 +0530

    Code Revised Completely

--- 18.returns_array_in_reverse_order/return_array_in_reverse_order.py
+++ 18.returns_array_in_reverse_order/return_array_in_reverse_order.py
@@ -1,18 +1,27 @@
-#18. Write a function that takes an unsorted
- #integer array as input and returns an array with the values in reverse order.
-import FindLenght
+Question :
+==========
+    Write a function that takes an unsorted integer array as input and returns an array with the values in reverse order.
+Input :
+=======
+     unsorted array 
+Ouput :
+=======
+     Reversed Unsorted array 
+Algorithm :
+===========
+      1. Initialize a empty array 
+      2. Using append function append all the elemnet in to the empty array using -i attribute value 
+      3. return that reversed unsorted array as output 
+Code :
+======
+ import FindLenght
 def returnContentInReverseOrder(array):
     ReversedArray = []
-    i = 1
+    iterator = 1
     lenght = FindLenght.count(array)
-    while (i!=lenght):
-        ReversedArray.append(array[-i])
-        i += 1
+    while (iterator!=lenght):
+        ReversedArray.append(array[-iterator])
+        iterator += 1
     ReversedArray.append(array[0])
 
     return ReversedArray
-print("---------------------------------")
-array = [3,1,6,3,8,9]
-print("The Reversed Array is")
-print(returnContentInReverseOrder(array))
-print("---------------------------------")
