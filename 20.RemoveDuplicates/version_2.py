383f1bf8787f6d096e5c2f7cf344b4a43bbc39c1
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-06 16:00:22 +0530

    Code Revised Completely

--- 20.RemoveDuplicates/version_2.py
+++ 20.RemoveDuplicates/version_2.py
@@ -1,11 +1,20 @@
-def remove_Dupli(array):
-    i = 0
-    b = []
-    while(i!=len(array)):
+Question :
+===========
+    Remove Duplicate without using Dictionary Data Structure 
+Input :
+=======
+    Unsorted Array Elements
+Ouptut :
+========
+    Array Elements not having duplicate values in it 
+Code :
+=======
+def removeDuplicate(array):
+    iterator = 0
+    newList = []
+    while(iterator!=len(array)):
         temp = array[i]
-        if temp not in b:
-            b.append(temp)
-        i += 1
-    return b 
-array = [7,1,2,4,8,9,7]
-print(remove_Dupli(array))
+        if temp not in newList:
+            newList.append(temp)
+        iterator += 1
+    return newList
