1130a663b1b4f1a181f672c0f132456d3f1b4ac4
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-06 16:10:26 +0530

    Code Revised Completely

--- 23.Prime_numbers/primeNumber.py
+++ 23.Prime_numbers/primeNumber.py
@@ -1,9 +1,8 @@
 import IsPrime
 
-def Prime_number(array):
-    for i in array:
-        if (IsPrime.Isprime(i)):
-            print(i)
-
-array = [2,6,8,1,0,32,6,9,12,7,3,0]
-Prime_number(array)
+def primeNumber(array):
+    primeList = []
+    for iterator in array:
+        if (IsPrime.Isprime(iterator)):
+            primeList.append(iterator)
+     return primeList
