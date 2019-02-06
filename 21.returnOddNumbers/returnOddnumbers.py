86876bf70d5579475e41333847dbda8d6dfee035
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-06 16:03:31 +0530

    Code Revised Completely

--- 21.returnOddNumbers/returnOddnumbers.py
+++ 21.returnOddNumbers/returnOddnumbers.py
@@ -1,11 +1,17 @@
-#21. Given an unsorted integer array as input,  return the number of odd numbers in it.
-
-def returnOddnumbers(array):
-    odd = []
-    for i in array:
-        if i%2 == 1:
-            odd.append(i)
-    return odd
-
-array = [5,3.2,33,2,2,7,4,9]
-print (returnOddnumbers(array))
+Question :
+===========
+    Given an unsorted integer array as input,  return the number of odd numbers in it.
+Input :
+=======
+    Unsorted Array 
+Output :
+========
+    return only the odd numbers as an array 
+Code :
+======
+def returnOddNumbers(array):
+    oddList = []
+    for iterator in array:
+        if iterator%2 == 1:
+            oddList.append(i)
+    return oddList
