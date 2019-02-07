7f7bde06c35869cc561769fe3d45cdc5cc344ec8
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-07 16:38:31 +0530

    Code Revised Completely

--- 25.2ndSmallestNumber/2ndSmallestNumber.py
+++ 25.2ndSmallestNumber/2ndSmallestNumber.py
@@ -1,15 +1,19 @@
+Question :
+==========
+    find minimum element from the list of elements
+ Input :
+========
+    List of Unsorted Array elements
+Output :
+========
+    Integer - minimum element from the list
+Code :
+======
 import find_min
-
 def secondsmallestNumber(array):
-    v = find_min.findMin(array)
-    for i in array:
-        if v == i:
-            array.remove(i)
-    maxi = find_min.findMin(array)
-    
-    return maxi
-
-print("==================================")
-array = [3,1,6,9,3]
-print(secondsmallestNumber(array)) 
-print("---------------------------------")
+    minimum_element = find_min.findMin(array)
+    for iterator in array:
+        if minimum_element == iterator:
+            array.remove(iterator)
+    minimum_element = find_min.findMin(array)
+    return minimum_element
