934cece4751fe0e1ed9254fe9a9521e5c3ef216c
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-08 22:31:34 +0530

    Code Revised Completely

--- 26.Find_kth_Largest_ele/find_kth_Largest_ele.py
+++ 26.Find_kth_Largest_ele/find_kth_Largest_ele.py
@@ -1,26 +1,24 @@
+Question : 
+==========
+       Find the kth largest Element in an array
+Input :
+=======
+    Unsorted Array - List in Python 
+Output :
+========
+    Kth Largest Element - Integer
+Code :
+======
 import Max
 import FindLenght
-import find_min
-def kth_largest_ele(array,kthele):
+def kth_largest_ele(array,kthelement):
     lenght = FindLenght.count(array)
-    if (lenght == kthele):
-        return find_min.findMin(array)
-    else:
-
-        iterator = 1
-        while(iterator!=kthele):
-            Largest_ele = Max.Max(array)
-            for j in array:
-                if (j == Largest_ele):
-                    array.remove(Largest_ele)
-            iterator += 1
-        Largest_ele = Max.Max(array)
-    return Largest_ele
-print("========================") 
-print()
-array = [5,1,9,7,2,8,3,6,4]
-kthele = 1
-#print()
-v = kth_largest_ele(array,kthele)
-print("The {0} Largest Element in the array is {1}".format(kthele,v))
-print("==================================================================")
+    iterator = 1
+    while(iterator!=kthelement):
+        largestElemnt = Max.Max(array)
+        for element in array:
+            if (element == largestElement):
+                 array.remove(largestElement)
+         iterator += 1
+    largestElement = Max.Max(array)
+    return largestElement
