b6f81a35bbf2197ed56eab1fb10d81779c1abe9d
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-08 22:34:05 +0530

    Code Revised Completely

--- 26.Find_kth_Largest_ele/FindLenght.py
+++ 26.Find_kth_Largest_ele/FindLenght.py
@@ -3,22 +3,17 @@ Question :
     To find the lenght of an array using slicing methodogy
 Input :
 =======
-    Unsorted Array Elements
+    Unsorted Array Elements - List in Python 
 Output:
 =======
-    Count the No of Element in the Array
-Algorithm:
-==========
-    1.Initialize the Cout variable to 0
-    2.Using the array Slicing concepts each and every element in an array is taken using While loop and the Count is incremented by 1
-    3.print the count variable 
+    Count the No of Element in the Array - Integer
+ 
 Code:
 =====
 def FindLenght(array):
     count = 0
     while(a[count:]):
         count += 1    
- 
     return count
 
  
