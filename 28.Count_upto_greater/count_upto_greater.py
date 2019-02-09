bc3ff21b9399ea7949b9127bfe3d300d16af50e3
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-09 11:11:43 +0530

    Code Revised Completely

--- 28.Count_upto_greater/count_upto_greater.py
+++ 28.Count_upto_greater/count_upto_greater.py
@@ -1,22 +1,19 @@
-#28. Given an unsorted integer array and an integer 
-# value X as input, return the count of the values greater than or equal to X.
-import FindLenght
-def validate(array,search_ele):
-    #flag = False
-    add = array[0] + array[1]
-    i = 2
-    Lenght = FindLenght.count(array)
-    
-    while (i!=Lenght):
-        add = add + array[i]
-        if (add >= search_ele):
-            #flag = True
-            break
+Question :
+==========
+    Given an unsorted integer array and an integer value X as input, return the count of the values greater than or equal to X.
+Input :
+=======
+    unsorted Array - List in Python 
+Output :
+========
 
-    return add
-print("-----------------------------------")
-print() 
-array = [5,1,9,7,2,8,3,6,4]
-search_ele = 25
-print(validate(array,search_ele))
-print("-----------------------------------")
+import FindLenght
+def validate(array,searchElementX):
+    #add = array[0] + array[1]
+    iterator = 0
+    Lenght = FindLenght.count(array)
+    add = 0
+    while (add>=searchElemntX):
+        add = add + array[i]
+        iterator += 1            
+    return add 
