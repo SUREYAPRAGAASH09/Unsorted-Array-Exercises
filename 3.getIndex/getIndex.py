bd33a360efcfed2139052f98fbd86c3954e548db
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-09 10:32:55 +0530

    Code Revised Completely

--- 3.getIndex/getIndex.py
+++ 3.getIndex/getIndex.py
@@ -1,18 +1,21 @@
-#3. Given an unsorted integer array A and an integer value X,
-#  return the index at which X is located in A or return -1 if it is not found in A.
-#import FindLenght
+Question :
+==========
+    Given an unsorted integer array A and an integer value X, return the index at which X is located in A or return -1 if it is not found in A.
+Input :
+=======
+    Unsorted array - List in Python 
+Output :
+========
+    Return Index - Integer 
+Code :
+=======
 def getIndex(array,indexFor):
     index = 0
-    i = 0
-    lennght = len(array)
-    while (i!=lennght):
-        if (array[i] == indexFor):
+    iterator = 0
+    lennght = len(array) # Lenght function is used - inbuilt method  
+    while (iterator!=lennght):
+        if (array[iterator] == indexFor):
             break
         index += 1
-        i += 1
+        iterator += 1
     return index
-print("-----------------------------------------")
-array = [7,4,5,7]
-indexFor = 5
-print(getIndex(array,indexFor))
-print("---------------------------------------------")
