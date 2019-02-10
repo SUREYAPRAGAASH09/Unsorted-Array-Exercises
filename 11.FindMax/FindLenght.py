708f90d1605d149f7ae3ba419afc85780d1b3996
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-10 23:20:18 +0530

    Code Revised Completely

--- 11.FindMax/FindLenght.py
+++ 11.FindMax/FindLenght.py
@@ -4,18 +4,12 @@ Question :
 
 Input :
 =======
-    Unsorted Array Elements
+    Unsorted Array Elements - list in Python
 
 Output:
 =======
-    Count the No of Element in the Array
-
-Algorithm:
-==========
-    1.Initialize the Cout variable to 0
-    2.Using the array Slicing concepts each and every element in an array is taken using While loop and the Count is incremented by 1
-    3.print the count variable 
-
+    Count the No of Element in the Array - Integer
+ 
 Code:
 =====
 
