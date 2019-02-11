602ffc1fd3e06ad701a77df7ce67f8a30103c647
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-11 22:25:35 +0530

    Code Revised Completely

--- 35.FindUniqueElements/findUnique.py
+++ 35.FindUniqueElements/findUnique.py
@@ -1,17 +1,23 @@
-#35. Given an unsorted integer array A containing 
-# elements from 0-9, find the number of unique elements.
+Question :
+==========
+    Given an unsorted integer array A containing  elements from 0-9, find the number of unique elements.
+Input :
+=======
+    Unsorted Integer Array
+Output :
+========
+    Get Uniques in integer Array
+Code :
+======
 import find_min
 import dictionaryOperations
 def findUnique(array):
     dictionary = dictionaryOperations.built_dictionary(array)
     listValues = list(dictionary.values())
     minimum_value = find_min.findMin(listValues)
-    for i in listValues:
-        if i == minimum_value :
-            b = (dictionaryOperations.find_key_from_dictionary(dictionary,i))
-            print(b)
-            dictionary.pop(b)
-
-array = [5,7,8,2,6,5,8]
-findUnique(array)
-#print(built_dictionary(array)) 
+    for iterator in listValues:
+        if iterator == minimum_value :
+            min = (dictionaryOperations.find_key_from_dictionary(dictionary,i))
+            print(min)
+            dictionary.pop(min)
+ 
