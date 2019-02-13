6d538f0cce2df18c2496a5f64b29d8546687faaa
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-13 18:58:32 +0530

    Code Revised Completely

--- 37.getIndexAfterRotationright/getindexRight.py
+++ 37.getIndexAfterRotationright/getindexRight.py
@@ -1,14 +1,18 @@
-#37. Given an unsorted integer array A, 
-# find the value that will be in 3rd position or index after 2 rotations to the right.
+Problem :
+=========
+    Given an unsorted integer array A,find the value that will be in 3rd position or index after 2 rotations to the right.
+Input :
+=======
+    An Unsorted Integer Array , 
+    Index of Type Integer , 
+    Rotation value of type integer
+Output :
+=========
+    Integer is extracted after rotation is made 
+Code :
+======
 import Rotateright
 
 def getIndexAfterrotationRight(array,index,rotation_value):
     afterRotation = Rotateright.swapRight(array,rotation_value)
     return afterRotation[index]
-
-print("=========================================================")
-array = [5,7,6,9,2,8]
-index = 3
-rotation_value = 2
-print(getIndexAfterrotationRight(array,index,rotation_value))
-print("==========================================================")
