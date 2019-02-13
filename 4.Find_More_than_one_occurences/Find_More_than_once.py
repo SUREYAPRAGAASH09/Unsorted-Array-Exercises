6644c8fbf63a77c880074b03763e6109aa16692e
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-13 19:21:11 +0530

    Code Revised Completely

--- 4.Find_More_than_one_occurences/Find_More_than_once.py
+++ 4.Find_More_than_one_occurences/Find_More_than_once.py
@@ -1,31 +1,31 @@
-# Given an unsorted integer array A and an integer value X, find if X is found more than once in A.
-#input = array 
-#output = if more than one element is found return true else false
-
-def count(a):
-    counti = 0
-    while(a[counti:]):
-        counti += 1
-
-    return counti
+Problem Statement :
+====================
+    Given an unsorted integer array A and an integer value X, find if X is found more than once in A.
+Input :
+=======
+    An Unsorted Integer Array
+  
+Output :
+========
+   True - if the element found more than once
+   False - if the element is not found more than once 
+Code :
+======
+def findLenght(a): 
+    count = 0
+    while(a[count:]):
+        count += 1
+    return count
 
 def find_more_than_once(array,search_elemnt):
     flag = False
-    i = 0
-    b = count(array)
-    counti = 0
-    while (i!=b):
-        if (array[i] == search_elemnt):
-            counti += 1
-        i += 1
-    if counti == 2:
+    iteratorValue = 0
+    lenght = count(array)
+    count = 0
+    while (iteratorValue!=lenght):
+        if (array[iteratorValue] == search_elemnt):
+            count += 1
+        iteratorValue += 1
+    if count == 2:
         flag = True
     return flag
-
-        
-    
-search_elemnt = 2
-print("---------------------------------------------")
-array = [7,9,1,2,4]
-print(find_more_than_once(array,search_elemnt))
-print("---------------------------------------------")
