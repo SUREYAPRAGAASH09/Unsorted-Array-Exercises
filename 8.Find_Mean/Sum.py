0132a7708cfd97633412f93f552370c0fcf28ec2
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-14 20:52:37 +0530

    Code Revised Completely

--- 8.Find_Mean/Sum.py
+++ 8.Find_Mean/Sum.py
@@ -1,17 +1,21 @@
-#Given an unsorted integer array A, find the sum of all the elements in A
-#input = array of any size
-#output = sum of all the element of the given array
-
+Problem Statement :
+===================    
+    Given an unsorted integer array A, find the sum of all the elements in A
+Input :
+=======    
+    Unsorted Integer array 
+Output :
+========    
+    sum of all the element - Integer 
+Code :
+======    
 import FindLenght
 
 def sum(array):
-    lenght_of_an_array = FindLenght.count(array)
-    sum = 0
-    i = 0
-    while(i!=lenght_of_an_array):
-        sum  = sum + array[i]
-        i += 1
-    return sum 
+    sumValue = 0
+    iteratorValue = 0
+    while(iteratorValue!=FindLenght.count(array)):
+        sumValue  =+ array[iteratorValue]
+        iteratorValue += 1
+    return sumValue 
 
-#array = [1,5,3,6,7,3]
-#print(sum(array))
