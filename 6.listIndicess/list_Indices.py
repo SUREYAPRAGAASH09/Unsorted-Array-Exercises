965a1876c070ebf291c37d6fafcbac932cde265a
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-14 20:43:14 +0530

    Code Revised Completely

--- 6.listIndicess/list_Indices.py
+++ 6.listIndicess/list_Indices.py
@@ -1,18 +1,23 @@
-#6. Given an unsorted integer array A and 
-#an integer value X, return the indices of the locations where X is found in A.
+Problem Statement :
+===================    
+    Given an unsorted integer array A and an integer value X, return the indices of the locations where X is found in A.
+Input :
+=======
+    Unsorted integer array
 
+Output :
+========
+    index Value - integer 
+    
+Code :
+======    
 def getIndex(array,indexFor):
     index_list = []
-    i = 0
-    index = 0
-    lennght = len(array)
-    while (i!=lennght):
+    iteratorValue = 0
+    indexValue = 0
+    while (i!=len(array):
         if (array[i] == indexFor):
             index_list.append(index)
         index += 1
         i += 1
     return index_list
-
-array = [7,4,5,7]
-indexFor = 7
-print(getIndex(array,indexFor))
