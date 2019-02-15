c89169182ad0f83cab4ab4e1b566be4f08bc03c0
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-15 23:27:45 +0530

    Code Revised Completely

--- 9.Most_occurence.py/find_most_occurence.py
+++ 9.Most_occurence.py/find_most_occurence.py
@@ -3,21 +3,21 @@ import Max
 import Equate
  
 def built_dictionary(array): # To Built a dictionary  
-    d = {} #creating a empty dictionary 
+    dictionary = {} #creating a empty dictionary 
     count = 0 
-    for i in array: 
+    for iterator in array: 
         count = 1 
-        if i in d: 
-            d[i] = d[i] + 1 
+        if iterator in dictionary: 
+            dictionary[iterator] = dictionary[iterator] + 1 
         else: 
-            d[i] = count  
-    return d 
+            dictionary[iterator] = count  
+    return dictionary 
 
 def find_key_from_dictionary(dictionary,max_element): # To find key from the value in an dictionary 
-    for k,v in dictionary.items(): 
-        if (v == max_element): 
-            key = k 
-    return key 
+    for key,value in dictionary.items(): 
+        if (value == max_element): 
+            keys = key 
+    return keys 
 
 def find_most_occureence(array): # the main function  
     dictionary = built_dictionary(array) 
@@ -28,10 +28,3 @@ def find_most_occureence(array): # the main function
         print ("the number {1} appears {0} times".format(max_element,key))
     else:
         print ("Can't find the most occurence of an element")
-
-
-#array = [1,2,5,3,2,4,5,5,5] 
-#array = [2,6,2,7,2,8,2]
-#array = [4,3,2,4,5,4]
-array = [1,2,3,4,5]
-find_most_occureence(array)
