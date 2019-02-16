a44e3a02560849f683802d0beb628beabc82ba14
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-16 22:34:43 +0530

    Code Revised Completely

--- 31.shiftRightby2bits/shiftRightby2.py
+++ 31.shiftRightby2bits/shiftRightby2.py
@@ -1,6 +1,6 @@
 Question :
 ==========
- Given an unsorted integer array as input, rotate the contents of the array to the right by 2 positions.
+      Given an unsorted integer array as input, rotate the contents of the array to the right by 2 positions.
 Input :
 =======
     Unsorted Integer Array 
@@ -11,11 +11,11 @@ Code :
 ======
 def swapRight(array):
     temp = 0
-    iterator2 = 0
-    while (iterator2!=2):
-        iterator = -1
-        while (iterator!=len(array)):
-            array[iterator],temp = temp,array[iterator]
-            iterator += 1
-        iterator2 += 1
+    shiftValue = 0
+    while (shiftValue!=2):
+        iteratorValue = -1
+        while (iteratorValue!=len(array)):
+            array[iteratorValue],temp = temp,array[iteratorValue]
+            iteratorValue += 1
+        shiftValue += 1
     return array
